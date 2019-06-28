92de65df786b414007eb87232f7636c8f9489d35
Narrakas <netkas@users.noreply.github.com>
2019-06-28 01:37:22 -0400

    Added proper meta tags

--- src/resources/sections/header.php
+++ src/resources/sections/header.php
@@ -5,3 +5,22 @@
 <link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 <link href="/assets/css/icons.css" rel="stylesheet" type="text/css" />
 <link href="/assets/css/style.css" rel="stylesheet" type="text/css" />
+
+<!-- Primary Meta Tags -->
+<title>CoffeeHouse API Dashboard</title>
+<meta name="title" content="CoffeeHouse API Dashboard">
+<meta name="description" content="Advanced Natrual Language Processing and Machine Learning chat dialog engine, powered by Intellivoid Cloud">
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://coffeehouse.intellivoid.info/">
+<meta property="og:title" content="CoffeeHouse API Dashboard">
+<meta property="og:description" content="Advanced Natrual Language Processing and Machine Learning chat dialog engine, powered by Intellivoid Cloud">
+<meta property="og:image" content="">
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://coffeehouse.intellivoid.info/">
+<meta property="twitter:title" content="CoffeeHouse API Dashboard">
+<meta property="twitter:description" content="Advanced Natrual Language Processing and Machine Learning chat dialog engine, powered by Intellivoid Cloud">
+<meta property="twitter:image" content="">
