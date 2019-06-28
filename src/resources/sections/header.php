ea034d907f7450f6c11837f9e58029732e62ae83
Narrakas <netkas@users.noreply.github.com>
2019-06-28 01:43:32 -0400

    Added favicon assets

--- src/resources/sections/header.php
+++ src/resources/sections/header.php
@@ -24,3 +24,15 @@
 <meta property="twitter:title" content="CoffeeHouse API Dashboard">
 <meta property="twitter:description" content="Advanced Natrual Language Processing and Machine Learning chat dialog engine, powered by Intellivoid Cloud">
 <meta property="twitter:image" content="">
+
+<link rel="apple-touch-icon" sizes="120x120" href="/assets/favicon/apple-touch-icon.png?v=YAmlY5waqg">
+<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png?v=YAmlY5waqg">
+<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png?v=YAmlY5waqg">
+<link rel="manifest" href="/assets/favicon/site.webmanifest?v=YAmlY5waqg">
+<link rel="mask-icon" href="/assets/favicon/safari-pinned-tab.svg?v=YAmlY5waqg" color="#876639">
+<link rel="shortcut icon" href="/assets/favicon/favicon.ico?v=YAmlY5waqg">
+<meta name="apple-mobile-web-app-title" content="CoffeeHouse">
+<meta name="application-name" content="CoffeeHouse">
+<meta name="msapplication-TileColor" content="#6f3727">
+<meta name="msapplication-config" content="/assets/favicon/browserconfig.xml?v=YAmlY5waqg">
+<meta name="theme-color" content="#ffffff">
