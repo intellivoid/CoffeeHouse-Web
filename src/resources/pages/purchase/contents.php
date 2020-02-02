eca369911ebd0c0827db910e41930b12443b6974
netkas <netkas@intellivoid.info>
2020-02-02 20:28:18 -0300

    Added checkout placeholder

--- src/resources/pages/purchase/contents.php
+++ src/resources/pages/purchase/contents.php
@@ -1,35 +1,38 @@
 <?php
     use DynamicalWeb\DynamicalWeb;
     use DynamicalWeb\HTML;
-
 ?>
 <!doctype html>
 <html lang="<?PHP HTML::print(APP_LANGUAGE_ISO_639); ?>">
     <head>
         <?PHP HTML::importSection('landing_headers'); ?>
-        <title>CoffeeHouse - Lydia Demo</title>
+        <link href="/assets/css/loader.css" rel="stylesheet">
+        <title>CoffeeHouse - Purchase Plan</title>
     </head>
 
-    <body data-spy="scroll" data-target="#ch-navbar" data-offset="0">
+    <body data-spy="scroll" data-target="#ch-navbar" data-offset="20">
         <?PHP HTML::importSection('landing_navbar'); ?>
-
-        <section class="section demo" id="demo">
-            <div class="bg-overlay"></div>
+        <section class="section generic">
             <div class="container">
                 <div class="row">
                     <div class="col-md-8 offset-md-2 text-white text-center">
-                        <img src="/assets/images/lydia_white_transparent.svg" class="img-fluid lydia_logo">
-                        <p class="mt-4 demo-subtitle mb-5 mb-5">Lydia is a advanced chat bot that actively learns from conversations and is capable of speaking in many languages without having to hard-configure anything about it.</p>
+                        <h1 class="generic-title mb-5">Confirm Purchase</h1>
                     </div>
                 </div>
             </div>
         </section>
-        <section class="section" id="chat_demo">
+        <section class="section">
             <div class="container">
-
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card m-b-20">
+                            <div class="card-body">
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
-
         <?PHP HTML::importSection('landing_footer'); ?>
         <?PHP HTML::importSection('landing_js'); ?>
     </body>
