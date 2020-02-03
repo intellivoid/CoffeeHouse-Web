92413b408b191c88809720ffd63751adfbfca43f
netkas <netkas@intellivoid.info>
2020-02-03 20:18:43 -0300

    Finally fixed that stupid bug

--- src/resources/pages/lydia_demo/contents.php
+++ src/resources/pages/lydia_demo/contents.php
@@ -23,15 +23,14 @@
         <section class="section demo" id="demo">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-8 offset-md-2 text-white text-center">
+                    <div class="col-md-8 offset-md-2 text-white text-center animated fadeIn slow">
                         <img src="/assets/images/lydia_white_transparent.svg" class="img-fluid lydia_logo">
                         <p class="mt-4 demo-subtitle mb-5 mb-5">Lydia is a advanced chat bot that actively learns from conversations and is capable of speaking in many languages without having to hard-configure anything about it.</p>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="lydiachat mb-5">
-                        <main class="lydiachat-chat" id="chat_content">
-                        </main>
+                    <div class="lydiachat mb-5 animated fadeInUp">
+                        <main class="lydiachat-chat" id="chat_content"></main>
                         <form class="lydiachat-inputarea" id="input_form">
                             <input class="form-control bg-dark text-white" autocomplete="off" type="text" id="user_input" placeholder="What's on your mind?" style="height: 40px;">
                             <button type="submit" class="btn btn-info btn-xs ml-2">
