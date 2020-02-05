346ff996b42f56140d87f5c4a3be38a889602bef
netkas <netkas@intellivoid.info>
2020-02-05 20:11:43 -0300

    Added Dashboard Status Logic

--- src/resources/pages/dashboard/contents.php
+++ src/resources/pages/dashboard/contents.php
@@ -1,8 +1,23 @@
 <?PHP
 
+    use CoffeeHouse\Abstracts\UserSubscriptionSearchMethod;
+    use CoffeeHouse\CoffeeHouse;
+    use CoffeeHouse\Exceptions\UserSubscriptionNotFoundException;
     use DynamicalWeb\Actions;
     use DynamicalWeb\DynamicalWeb;
     use DynamicalWeb\HTML;
+    use DynamicalWeb\Runtime;
+    use IntellivoidAPI\Abstracts\SearchMethods\AccessRecordSearchMethod;
+    use IntellivoidAPI\Exceptions\AccessRecordNotFoundException;
+    use IntellivoidAPI\IntellivoidAPI;
+    use IntellivoidSubscriptionManager\Abstracts\SearchMethods\SubscriptionSearchMethod;
+    use IntellivoidSubscriptionManager\Exceptions\SubscriptionNotFoundException;
+    use IntellivoidSubscriptionManager\IntellivoidSubscriptionManager;
+    use IntellivoidSubscriptionManager\Objects\Subscription\Feature;
+
+    Runtime::import('CoffeeHouse');
+    Runtime::import('IntellivoidSubscriptionManager');
+    Runtime::import('IntellivoidAPI');
 
     HTML::importScript('require_auth');
     HTML::importScript('check_subscription');
@@ -21,6 +36,122 @@
     if(WEB_SUBSCRIPTION_ACTIVE == false)
     {
         Actions::redirect(DynamicalWeb::getRoute('index') . '#pricing');
+    }
+
+    // DASHBOARD LOGIC
+    if(isset(DynamicalWeb::$globalObjects['intellivoid_api']) == false)
+    {
+        /** @var IntellivoidAPI $IntellivoidAPI */
+        $IntellivoidAPI = DynamicalWeb::setMemoryObject('intellivoid_api', new IntellivoidAPI());
+    }
+    else
+    {
+        /** @var IntellivoidAPI $IntellivoidAPI */
+        $IntellivoidAPI = DynamicalWeb::getMemoryObject('intellivoid_api');
+    }
+
+    if(isset(DynamicalWeb::$globalObjects['intellivoid_subscription_manager']) == false)
+    {
+        /** @var IntellivoidSubscriptionManager $IntellivoidSubscriptionManager */
+        $IntellivoidSubscriptionManager = DynamicalWeb::setMemoryObject('intellivoid_subscription_manager', new IntellivoidSubscriptionManager());
+    }
+    else
+    {
+        /** @var IntellivoidSubscriptionManager $IntellivoidSubscriptionManager */
+        $IntellivoidSubscriptionManager = DynamicalWeb::getMemoryObject('intellivoid_subscription_manager');
+    }
+
+    if(isset(DynamicalWeb::$globalObjects['coffeehouse']) == false)
+    {
+        /** @var CoffeeHouse $CoffeeHouse */
+        $CoffeeHouse = DynamicalWeb::setMemoryObject('coffeehouse', new CoffeeHouse());
+    }
+    else
+    {
+        /** @var CoffeeHouse $CoffeeHouse */
+        $CoffeeHouse = DynamicalWeb::getMemoryObject('coffeehouse');
+    }
+
+    try
+    {
+        $UserSubscription = $CoffeeHouse->getUserSubscriptionManager()->getUserSubscription(
+            UserSubscriptionSearchMethod::byAccountID, WEB_ACCOUNT_ID
+        );
+    }
+    catch (UserSubscriptionNotFoundException $e)
+    {
+        Actions::redirect(DynamicalWeb::getRoute('service_error', array(
+            'error_type' => 'rd_us_not_found'
+        )));
+    }
+    catch(Exception $e)
+    {
+        Actions::redirect(DynamicalWeb::getRoute('service_error', array(
+            'error_type' => 'rd_us_cluster_error'
+        )));
+    }
+
+
+    try
+    {
+        $Subscription = $IntellivoidSubscriptionManager->getSubscriptionManager()->getSubscription(
+            SubscriptionSearchMethod::byId, $UserSubscription->SubscriptionID
+        );
+    }
+    catch (SubscriptionNotFoundException $e)
+    {
+        Actions::redirect(DynamicalWeb::getRoute('service_error', array(
+            'error_type' => 'rd_s_not_found'
+        )));
+    }
+    catch(Exception $e)
+    {
+        Actions::redirect(DynamicalWeb::getRoute('service_error', array(
+            'error_type' => 'rd_s_cluster_error'
+        )));
+    }
+
+    try
+    {
+        $AccessRecord = $IntellivoidAPI->getAccessKeyManager()->getAccessRecord(
+            AccessRecordSearchMethod::byId, $UserSubscription->AccessRecordID
+        );
+    }
+    catch (AccessRecordNotFoundException $e)
+    {
+        Actions::redirect(DynamicalWeb::getRoute('service_error', array(
+            'error_type' => 'rd_ar_not_found'
+        )));
+    }
+    catch(Exception $e)
+    {
+        Actions::redirect(DynamicalWeb::getRoute('service_error', array(
+            'error_type' => 'rd_ar_cluster_error'
+        )));
+    }
+
+    $ConfiguredLydiaSessions = "Unknown";
+    $UsedLydiaSessions = "Unknown";
+
+    /** @var Feature $feature */
+    foreach($Subscription->Properties->Features as $feature)
+    {
+        switch($feature->Name)
+        {
+            case 'LYDIA_SESSIONS':
+                $ConfiguredLydiaSessions = (int)$feature->Value;
+                break;
+        }
+    }
+
+    if(isset($AccessRecord->Variables['LYDIA_SESSIONS']))
+    {
+        $UsedLydiaSessions = (int)$AccessRecord->Variables['LYDIA_SESSIONS'];
+
+        if($UsedLydiaSessions > $ConfiguredLydiaSessions)
+        {
+            $UsedLydiaSessions = $ConfiguredLydiaSessions;
+        }
     }
 
 ?>
