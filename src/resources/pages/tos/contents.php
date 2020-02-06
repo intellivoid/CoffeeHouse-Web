0191200509cefd86229e3702cd1ac188969d8a0c
netkas <netkas@intellivoid.info>
2020-02-05 23:45:56 -0300

    Created translation for 'tos'

--- src/resources/pages/tos/contents.php
+++ src/resources/pages/tos/contents.php
@@ -7,38 +7,32 @@
     <head>
         <?PHP HTML::importSection('landing_headers'); ?>
         <link href="/assets/css/loader.css" rel="stylesheet">
-        <title>CoffeeHouse - Terms of Service</title>
+        <title><?PHP HTML::print(TEXT_PAGE_TITLE); ?></title>
     </head>
-
     <body data-spy="scroll" data-target="#ch-navbar" data-offset="20">
         <?PHP HTML::importSection('landing_navbar'); ?>
         <section class="section generic" id="tos">
             <div class="container">
                 <div class="row">
                     <div class="col-md-8 offset-md-2 text-white text-center">
-                        <h1 class="generic-title mb-5">Terms of Service</h1>
+                        <h1 class="generic-title mb-5"><?PHP HTML::print(TEXT_PAGE_HEADER); ?></h1>
                     </div>
                 </div>
             </div>
         </section>
-
         <section class="section">
             <div class="container">
                 <div class="row">
                     <div class="col-12">
-
                         <div class="card m-b-20">
                             <div class="card-body">
                                 <?PHP HTML::importMarkdown('tos'); ?>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
         </section>
-
-
         <?PHP HTML::importSection('landing_footer'); ?>
         <?PHP HTML::importSection('landing_js'); ?>
     </body>
