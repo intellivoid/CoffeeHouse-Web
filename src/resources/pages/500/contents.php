fa6a4cdf4dcaccadd3633e925f59c5d69acc3990
netkas <netkas@intellivoid.info>
2020-02-05 22:35:13 -0300

    Created translation for '500'

--- src/resources/pages/500/contents.php
+++ src/resources/pages/500/contents.php
@@ -7,39 +7,34 @@
     <head>
         <?PHP HTML::importSection('landing_headers'); ?>
         <link href="/assets/css/loader.css" rel="stylesheet">
-        <title>CoffeeHouse - 500 Internal Server Error</title>
+        <title><?PHP HTML::print(TEXT_PAGE_TITLE); ?></title>
     </head>
-
     <body data-spy="scroll" data-target="#ch-navbar" data-offset="20">
         <?PHP HTML::importSection('landing_navbar'); ?>
         <section class="section error">
             <div class="container">
                 <div class="row">
                     <div class="col-md-8 offset-md-2 text-white text-center">
-                        <h1 class="error-title m-2">500</h1>
-                        <h1 class="error-subtitle mb-5">Server made an oopsy, but we can't show you why. Let our staff know instead!</h1>
+                        <h1 class="error-title m-2"><?PHP HTML::print(TEXT_PAGE_HEADER); ?></h1>
+                        <h1 class="error-subtitle mb-5"><?PHP HTML::print(TEXT_PAGE_DESC); ?></h1>
                     </div>
                 </div>
             </div>
         </section>
-
         <section class="section">
             <div class="container">
                 <div class="card">
                     <div class="card-body">
-
                         <div class="ex-page-content text-center mt-4">
-                            <h1 class="mb-4">What can i do?</h1>
-
-                            <a class="btn btn-outline-primary mb-5 waves-effect waves-light" href="<?PHP DynamicalWeb::getRoute('index', array(), true); ?>">Go Home</a>
+                            <h1 class="mb-4"><?PHP HTML::print(TEXT_CARD_CONTENT_HEADER); ?></h1>
+                            <a class="btn btn-outline-primary mb-5 waves-effect waves-light" href="<?PHP DynamicalWeb::getRoute('index', array(), true); ?>">
+                                <?PHP HTML::print(TEXT_HOME_BUTTON); ?>
+                            </a>
                         </div>
-
                     </div>
                 </div>
             </div>
         </section>
-
-
         <?PHP HTML::importSection('landing_footer'); ?>
         <?PHP HTML::importSection('landing_js'); ?>
     </body>
