5a56af52a75474ee3a850c24b0c3482893a2d893
netkas <netkas@intellivoid.info>
2020-06-05 14:57:03 -0400

    Updated force cache

--- src/resources/sections/landing_js.php
+++ src/resources/sections/landing_js.php
@@ -1,4 +1,4 @@
-<script src="/assets/js/jquery.min.js"></script>
+<script src="/assets/js/jquery.min.js?v=351"></script>
 <script src="/assets/js/bootstrap.bundle.min.js"></script>
 <script src="/assets/js/jquery.sticky.js"></script>
 <script src="/assets/js/jquery.easing.min.js"></script>
